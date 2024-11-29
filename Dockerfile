ab72e59256ed2ee04c7303c3b4fc28a5f6457084
Andrea <00075919@uca.edu.sv>
2024-11-29 15:27:27 -0600

    Add: files

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+# Use a lightweight Node image to build the app
+FROM node:16-alpine
+
+# Create app directory
+WORKDIR /app
+
+# Install dependencies
+COPY package.json ./
+RUN npm install
+
+# Copy and build the app
+COPY . .
+
+# Use an Nginx image to serve the app
+EXPOSE 80
+CMD ["npm","run","start"]
---
+++ src/components/Todo.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
+export const Todo = ({ task, deleteTodo, editTodo, toggleComplete }) => {
+  return (
+    <div className="Todo">
+      <p
+        className={`${task.completed ? 'completed' : 'incompleted'}`}
+        onClick={() => toggleComplete(task.id)}
+      >
+        {task.task}
+      </p>
+      <div>
+        <button onClick={() => editTodo(task.id)} aria-label="edit"> 
+          <FontAwesomeIcon
+            className="edit-icon"
+            icon={faPenToSquare}
+          />
+        </button>
+        <button onClick={() => deleteTodo(task.id)} aria-label="delete">
+          <FontAwesomeIcon
+            className="delete-icon"
+            icon={faTrash}
+          />
+        </button>
+      </div>
+    </div>
+  );
+};
