4c3a8480382809f889ffa3e4144d830cce28b519
Andrea <00075919@uca.edu.sv>
2024-12-02 17:21:10 -0600

    try5

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -42,7 +42,7 @@ export const TodoWrapper = () => {
 
   return (
     <div className="TodoWrapper">
-      <h1>Estoy guapa!</h1>
+      <h1>Estoy guapaaa!</h1>
       <TodoForm addTodo={addTodo} />
       {/* display todos */}
       {todos.map((todo) =>
